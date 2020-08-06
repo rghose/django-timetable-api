7d83ac52b5b2989a474364d65ffebe8d251ab69b
Rahul Ghose <hansum.rahul@gmail.com>
2020-08-06 21:49:14 +0530

    added more tests and working API now

--- timetable/webservice/tests.py
+++ timetable/webservice/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import RequestFactory
 
 from .views import handle_interview_request, Constants
-from .models import Student, Trainer, Interview
+from .models import Student, Trainer, Interview, AvailabilitySlot
 
 import json
 import datetime
@@ -73,15 +73,14 @@ class InterviewRequestWithMockData(TestCase):
         s = Student(name="test")
         s.save()
         rf = RequestFactory()
-        data={"studentId": 1, "startDateTime": "2020-06-05T13:00:00", "endDateTime": "2020-06-05T14:00:00" }
+        data={"studentId": s.id, "startDateTime": "2020-06-05T13:00:00", "endDateTime": "2020-06-05T14:00:00" }
         request = rf.post('/api/interview',content_type='application/json', data=json.dumps(data))
         response = handle_interview_request(request)
-        self.assertEqual(response.status_code, 200)
-        content_json = json.loads(response.content)
-        self.assertEqual(content_json["interview"], False)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content.decode(), Constants.ERROR_NO_SLOTS)
 
     def test_max_interviews_exceeded(self):
-        old_value = Constants.MAX_INTERVIEWS_ALLOWED
+        old_value_max_interviews = Constants.MAX_INTERVIEWS_ALLOWED
         Constants.MAX_INTERVIEWS_ALLOWED = 3
         s = Student(name="test")
         s.save()
@@ -98,10 +97,10 @@ class InterviewRequestWithMockData(TestCase):
         response = handle_interview_request(request)
         self.assertEqual(response.status_code, 410)
         self.assertEqual(str(response.content.decode()), Constants.ERROR_MAX_INTERVIEWS_EXCEEDS)
-        Constants.MAX_INTERVIEWS_ALLOWED = old_value
+        Constants.MAX_INTERVIEWS_ALLOWED = old_value_max_interviews
 
     def test_max_tests_flunked(self):
-        s = Student(name="test")
+        s = Student(name="test2")
         s.save()
         t = Trainer(name="trainer2")
         t.save()
@@ -116,3 +115,53 @@ class InterviewRequestWithMockData(TestCase):
         response = handle_interview_request(request)
         self.assertEqual(response.status_code, 410)
         self.assertEqual(str(response.content.decode()), Constants.ERROR_MAX_BAD_INTERVIEWS)
+    
+    def test_student_with_previous_interviews_from_other_trainers(self):
+        s = Student(name="student")
+        s.save()
+        for i in range(0,4):
+            t = Trainer(name="trainer{}".format(i))
+            t.save()
+            f = datetime.datetime.fromisoformat("2020-05-01T13:00:00")
+            e = datetime.datetime.fromisoformat("2020-05-01T14:00:00")
+            iv = Interview(student=s, trainer=t, grade=2, fromTime=f, toTime=e)
+            iv.save()
+            fa = datetime.datetime.fromisoformat("2020-08-05T10:00:00")
+            ta = datetime.datetime.fromisoformat("2020-08-05T20:00:00")
+            a = AvailabilitySlot(trainer=t, fromTime=fa, toTime=ta)
+            a.save()
+        rf = RequestFactory()
+        data={"studentId": s.id, "startDateTime": "2020-08-05T13:00:00", "endDateTime": "2020-08-05T14:00:00" }
+        request = rf.post('/api/interview',content_type='application/json', data=json.dumps(data))
+        response = handle_interview_request(request)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.content.decode(), Constants.ERROR_NO_SLOTS)
+
+    def test_student_with_new_trainer_slot(self):
+        s = Student(name="student")
+        s.save()
+        for i in range(0,4):
+            t = Trainer(name="trainer{}".format(i))
+            t.save()
+            f = datetime.datetime.fromisoformat("2020-05-01T13:00:00")
+            e = datetime.datetime.fromisoformat("2020-05-01T14:00:00")
+            iv = Interview(student=s, trainer=t, grade=2, fromTime=f, toTime=e)
+            iv.save()
+            fa = datetime.datetime.fromisoformat("2020-08-05T10:00:00")
+            ta = datetime.datetime.fromisoformat("2020-08-05T20:00:00")
+            a = AvailabilitySlot(trainer=t, fromTime=fa, toTime=ta)
+            a.save()
+        trainer_name = "newTrainer"
+        new_trainer = Trainer(name=trainer_name)
+        new_trainer.save()
+        fav = datetime.datetime.fromisoformat("2020-08-05T10:00:00")
+        tav = datetime.datetime.fromisoformat("2020-08-05T20:00:00")
+        av = AvailabilitySlot(trainer=new_trainer, fromTime=fav, toTime=tav)
+        av.save()
+        rf = RequestFactory()
+        data={"studentId": s.id, "startDateTime": "2020-08-05T13:00:00", "endDateTime": "2020-08-05T14:00:00" }
+        request = rf.post('/api/interview',content_type='application/json', data=json.dumps(data))
+        response = handle_interview_request(request)
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.content.decode())
+        self.assertEqual(json_response["trainer"], trainer_name)
--- timetable/webservice/views.py
+++ timetable/webservice/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.db import transaction
 from django.http import HttpResponse, JsonResponse, \
-    HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseGone
+    HttpResponseNotAllowed, HttpResponseBadRequest, HttpResponseGone,\
+    HttpResponseNotFound
 
 from .models import Student, Interview, AvailabilitySlot
 
@@ -12,6 +14,7 @@ class Constants:
     ERROR_METHOD_NOT_ALLOWED = "method not allowed"
     ERROR_MAX_INTERVIEWS_EXCEEDS = "max interviews exhausted"
     ERROR_MAX_BAD_INTERVIEWS = "too many bad interviews"
+    ERROR_NO_SLOTS = "no slots available"
     MAX_INTERVIEWS_ALLOWED = 15
     GRADE_UNGRADED = -1
 
@@ -35,6 +38,29 @@ def parse_req_body(body: str):
     student = get_object_or_404(Student, pk=booking_req["studentId"])
     return (student, start, end)
 
+@transaction.atomic
+def book_time_slot(slot: AvailabilitySlot, student: Student, student_start: datetime, student_end: datetime) -> str:
+    """
+    Given a query set of AvailabilitySlots book a time slot atomically
+    - Create before and after time slots of the trainer whose slot is being booked
+    - Create an interview item in database to signify that an interview was booked
+    - Delete the selected slot from availability database
+    - Return the trainer name
+    """
+    locked_slot = AvailabilitySlot.objects.select_for_update().get(pk=slot.id)
+    trainer = locked_slot.trainer
+    trainer_name = str(trainer)
+    if locked_slot.fromTime != student_start:
+        before_slot = AvailabilitySlot(fromTime=locked_slot.fromTime, toTime=student_start, trainer=trainer)
+        before_slot.save()
+    if locked_slot.toTime != student_end:
+        after_slot = AvailabilitySlot(fromTime=student_end, toTime=locked_slot.toTime, trainer=trainer)
+        after_slot.save()
+    iv = Interview(student=student, trainer=trainer, fromTime=student_start, toTime=student_end, grade=Constants.GRADE_UNGRADED)
+    iv.save()
+    locked_slot.delete()
+    return trainer_name
+
 def handle_interview_request(request):
     """
     sample data format:
@@ -46,7 +72,6 @@ def handle_interview_request(request):
     """
     if request.method != "POST":
         return HttpResponseNotAllowed(Constants.ERROR_METHOD_NOT_ALLOWED)
-    confirmed = False
     try:
         student, start, end = parse_req_body(request.body)
     except Exception as e:
@@ -59,7 +84,7 @@ def handle_interview_request(request):
     if total_interviews >= 2 and grade_flunked(interviews[0]) and grade_flunked(interviews[1]):
         return HttpResponseGone(Constants.ERROR_MAX_BAD_INTERVIEWS)
     previous_interviewers = []
-    if total_interviews >= 1:
-        print("interviews:",interviews)
-        #previous_interviewers = interviews.trainer
-    return JsonResponse({"interview": confirmed})
+    result = AvailabilitySlot.objects.filter(fromTime__lte=start, toTime__gte=end).exclude(trainer__in=interviews.values_list('trainer', flat=True))
+    if len(result) == 0:
+        return HttpResponseNotFound(Constants.ERROR_NO_SLOTS)
+    return JsonResponse({"trainer": book_time_slot(result[0], student, start, end)})
